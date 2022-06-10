2a338db993fe199013792b523131436f7fb6b640
kyo <mathis.charretier.42@gmail.com>
2022-06-10 16:47:18 +0000

    done

--- pages/api/vocapi.js
+++ pages/api/vocapi.js
@@ -1,11 +1,11 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    const filePath = path.join(process.cwd(), "data", "listes.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
-    res.status(200).json({ data: 5 });
-  }
-}
+// export default function handler(req, res) {
+//   if (req.method === "GET") {
+//     const filePath = path.join(process.cwd(), "data", "listes.json");
+//     const fileData = fs.readFileSync(filePath);
+//     const data = JSON.parse(fileData);
+//     res.status(200).json({ data: 5 });
+//   }
+// }
